93d4e6ae8383a01b46155b4a406a6d19a38961ff
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 21:52:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  
+  def initialize(title)
+    
+    @title = title
+    
+  end
+  attr_accessor :title, :author, :page_count, :genre
+  
+   def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
